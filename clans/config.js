25c422e2065d8dde5aa079d5a965989cbb649553
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2021-12-23 01:53:46 +0300

    1

---
+++ clans/config.js
@@ -0,0 +1,19 @@
+[
+        {
+                "name": "iterambere",
+                "url": "SCP742.html",
+                "image": "-.jpg"
+        },
+
+        {
+                "name": "Мифический рассвет",
+                "url": "p0pb0b.html",
+                "image": "-.jpg"
+        },
+
+        {
+                "name": "COHA",
+                "url": "COHA",
+                "image": "-.jpg"
+        }
+]
