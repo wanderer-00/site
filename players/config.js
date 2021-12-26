cc641b5bf36e10b4066b8fdbf7f1b7ad73ea02a2
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2021-12-26 14:32:42 +0300

    0

--- players/config.js
+++ players/config.js
@@ -11,5 +11,26 @@
                 "clan": "-",
                 "url": "p0pb0b.html",
                 "image": "p0pb0b.png"
+        },
+
+        {
+                "name": "SCP_bot_1",
+                "clan": "-",
+                "url": "SCP_bot_1.html",
+                "image": " .png"
+        },
+
+        {
+                "name": "SCP_bot_2",
+                "clan": "-",
+                "url": "SCP_bot_2.html",
+                "image": " .png"
+        },
+
+        {
+                "name": "twink0",
+                "clan": "-",
+                "url": "twink0.html",
+                "image": " .png"
         }
 ]
---
+++ schema/config.js
@@ -0,0 +1,16 @@
+[
+        {
+                "name": "pick",
+                "spell":[
+                    
+                ],
+                "image": "SCP742.png"
+        },
+
+        {
+                "name": "p0pb0b",
+                "clan": "-",
+                "url": "p0pb0b.html",
+                "image": "p0pb0b.png"
+        }
+]
